c1823f4f3bc54839b5fe571beab88f38dc940257
agez0s <eduardo.cordeiro@protonmail.com>
2021-12-19 09:38:35 -0300

    Primeiro commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav className="flex bg-black text-white justify-between">
+           Teste 
+        </nav>
+    )
+}
+
+export default Header
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+
+const Home = () => {
+    return (
+        <>
+        <Header />
+            Sou a Main
+        </>
+        
+    )
+}
+
+export default Home
